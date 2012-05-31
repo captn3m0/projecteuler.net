a21bf1dcb053a150bcc4a5e5dc17c96b9263d116
Abhay Rana <capt.n3m0@gmail.com>
2012-05-31 18:43:37 +0530

    Did #12

--- 12.js
+++ 12.js
@@ -1,15 +1,25 @@
+var _=require('underscore');
 function numberOfDivisors(n){
+  var a= [];
   var c=2;//2for 1 and n
-  for(var i=2;i<n;i++)
-    if(n%i==0)
-      c++;
-  return c;
+  for(var i=2;i<Math.sqrt(n)+1;i++)
+    if(n%i==0){
+		a.push(i);
+		a.push(n/i);
+	}
+  return c+_.unique(a).length;
 }
 
-function tr(i){ 
-  var j=i++;
-  return (i*j)/2;
+//Return the ith triangle number
+function tr(i){
+  return (i*++i)/2;
 }
-var x= process.argv[2];
-console.log('triangle number is'+tr(x));
-console.log(numberOfDivisors(tr(x)));
+
+for(n=1;;n++){
+	var nthTriangleNumber = tr(n);
+	var div = numberOfDivisors(nthTriangleNumber);
+	if(div >500){
+		console.log(nthTriangleNumber);
+		break;
+	}
+}
